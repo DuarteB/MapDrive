9c2d9eacf2fc0e1e711a23dae4cbbf4551eaf57f
Eduardo Duarte <eduarte.dev@gmail.com>
2021-10-29 17:17:09 -0300

    Script para mapeamento de rede

--- shell.js
+++ shell.js
@@ -1,12 +1,64 @@
 const { exec } = require('child_process');
-const path = require('path');
+const createShortcut = require('create-desktop-shortcuts');
 
-console.log('|Exibindo Echo|')
+const mapear = (unidade, pasta, nomeSetor) => {
+    console.log(`Mapeando ${unidade} no setor: ${nomeSetor}`)
 
-exec("echo Deu certo ", (err, stdout) => {
-    if(err) {
-        console.error(`err: ${err}`);
-    }
-    console.log(`stdout: ${stdout}`);
-    console.log("|Echo exibido com sucesso|")
-})
+    exec(`net use ${unidade}: "\\\\fs\\${pasta}"`, (err, stdout) => {
+        if(err) {
+            console.error(`err: ${err}`);
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log("Mapeado com sucesso")
+    })
+
+    setTimeout(() => {
+        createShortcut({
+            windows:{
+                filePath: `${unidade}:\\`,
+                outputPath: `"%userprofile%\\desktop\\${nomeSetor}.lnk"`,
+                name: `${nomeSetor}`,
+            }
+        })
+    }, 1000)
+}
+
+const deleteMap = (unidade, nomeSetor) => {
+    console.log(`Excluindo mapeamento do disco: ${unidade}`)
+
+    exec(`del "%userprofile%\\desktop\\${nomeSetor}.lnk"`, (err, stdout) => {
+        if(err) {
+            console.error(err)
+        }
+        console.log(`stdout: ${stdout}`)
+    })
+
+    setTimeout(() => {
+        exec(`net use ${unidade}: /delete`, (err, stdout) => {
+            if(err) {
+                console.error(err)
+            }
+            console.log(`stdout: ${stdout}`)
+            console.log(`Disco ${unidade}: excluído com sucesso`)
+        })
+    }, 1000)
+}
+
+// mapear("x", "comercial", "Setor Comercial")
+deleteMap("x", "Setor Comercial")
+
+// exec("net use g: \\\\fs\\area_pessoal", (err, stdout) => {
+//     if(err) {
+//         console.error(`err: ${err}`);
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.log("Mapeado com sucesso")
+// })
+
+// exec("net use r: \\\\fs\\compras", (err, stdout) => {
+//     if(err) {
+//         console.error(`err: ${err}`);
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.log("Mapeado com sucesso")
+// })
